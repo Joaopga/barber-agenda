d1df628f45c55a0394bf9db0557d48ee3aab93c7
João Pedro Garcia <joaoplimagarcia@gmail.com>
2026-04-24 18:18:51 -0300

    Remove SVG tag and adjust JS files

--- js/main.js
+++ js/main.js
@@ -243,19 +243,8 @@ function renderUsers(searchTerm = '') {
       <td><span class="badge ${user.role.toLowerCase()}">${getRoleLabel(user.role)}</span></td>
       <td class="actions-cell">
         <button class="btn-icon edit" onclick="openUserModal(${user.id})" title="Editar">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/>
-            <path d="m15 5 4 4"/>
-          </svg>
         </button>
         <button class="btn-icon delete" onclick="openDeleteModal(${user.id})" title="Excluir">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M3 6h18"/>
-            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/>
-            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/>
-            <line x1="10" x2="10" y1="11" y2="17"/>
-            <line x1="14" x2="14" y1="11" y2="17"/>
-          </svg>
         </button>
       </td>
     `;
--- js/uList.js
+++ js/uList.js
@@ -1,19 +1,3 @@
 import { checkLogin } from "./checkLogin.js";
 
 checkLogin()
-/*
-  let users = [];
-  let currentUserId = null;
-  let isEditing = false;
-
-  // Start login
-  document.addEventListener("DOMContentLoaded", () => {
-    // Check which page we're on and initialize accordingly
-    if (document.getElementById("loginForm")) {
-      initLoginPage();
-    }
-
-    if (document.getElementById("usersTableBody")) {
-      initUsersPage();
-    }
-  });*/
